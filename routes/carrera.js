11c3956504d1c0e8138f4cc6465a39d84f412c90
Yahir2023 <nohramirezalejandro@gmail.com>
2024-03-01 09:22:36 -0500

    ACTUAL

--- routes/carrera.js
+++ routes/carrera.js
@@ -9,9 +9,18 @@ const {connection} = require("../config/config.db");
 
 //POST CARRERA
 //RECIBE UN ID PARA SEPARAR LAS CARRERAS Y VISUALIZAR SUS ALUMNOS SEGUN SU ID CARRERA
-const getCarrera = (request,response) => {
+const getCarreras = (request,response) => {
+    connection.query("SELECT * FROM tbl_carrera",
+    (error,results) => {
+        if(error)
+            throw error;
+        response.status(201).json(results);
+    });
+};
+app.route("/carrera").get(getCarreras);
+
+const getCarreraId = (request,response) => {
     const id = request.params.id;
-    //console.log(id):return false;
     connection.query("SELECT tbl_carrera.Carrera,tbl_alumno.ID_Alumno,tbl_alumno.Nombre,tbl_alumno.Apellido,tbl_alumno.Edad,tbl_alumno.Email FROM tbl_carrera INNER JOIN tbl_alumno WHERE tbl_alumno.FK_Carrera = ?",
     [id],
     (error,results) => {
@@ -20,6 +29,6 @@ const getCarrera = (request,response) => {
         response.status(201).json(results);
     });
 };
-app.route("/carrera/:id").get(getCarrera);
+app.route("/carrera/:id").get(getCarreraId);
 //SI
 module.exports = app;
